4b16a2a3a8c5a899b1cc7c21490121234f3c1577
Spring Boot <v.bharathbsk97@gmail.com>
2026-07-23 16:06:02 +0530

    Base URL undo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/", // 👈 Add this line
+  base: "/bskcoding/", // 👈 Add this line
 
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 
